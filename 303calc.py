10d02a977a5683050a6733c899c3728e5226422a
Martin Bieker <martin.bieker@tu-dortmund.de>
2013-12-11 15:17:11 +0100

    Bilder hinzugefuegt

--- 303calc.py
+++ 303calc.py
@@ -10,24 +10,43 @@ from matplotlib.pyplot import  *
 from scipy.optimize import curve_fit
 from uncertainties import *
 
-data1 = loadtxt('Messwerte/aufg2', unpack='True')
-data1[0] = data1[0]*pi/180
-def cos_fit(phi,U_0,C):
-    return 2*U_0*cos(phi)/pi+C
-params , cov = curve_fit(cos_fit,data1[0],data1[1],(-5.3,0))
-plot(data1[0],data1[1],'x')
 
+
+data = loadtxt('Messwerte/aufg2', unpack='True')
+data[0] = data[0]*pi/180
+data[1] = data[1]/2
+def cos_fit(phi,A):
+    return A*cos(phi)/pi
+params , cov = curve_fit(cos_fit,data[0],data[1])
+plot(data[0],data[1],'x')
+xlabel("Phasenverschiebung [rad]")
+ylabel(r"$U_{out} [V]$")
+grid(True)
 phi = linspace(0,2*pi)
+A = ufloat(params[0],cov[0][0])
 
-U_0 = ufloat(params[0],cov[0][0])
-C = ufloat(params[1],cov[1][1])
 
-print('Amplitude : %s und Eichfehler %s' % (U_0 , C))
 
-plot(phi, cos_fit(phi, params[0],params[1]))
+
+plot(phi, cos_fit(phi, params[0]))
+show()
 close()
 
+#Verlauf bei verrasuchtem Signal
+data = loadtxt("Messwerte/aufg3")
+phase_shift = data[0]*pi/180.
+output = data[1]/data[2]
 
+
+params , cov = curve_fit(cos_fit,phase_shift,output)
+phi = linspace(0,2*pi)
+A = ufloat(params[0],cov[0][0])
+plot(phi, cos_fit(phi, params[0]))
+
+
+plot(phase_shift,output, 'x')
+show()
+close()
 
 
 ## Aufg 4 LED Verlauf
@@ -37,14 +56,15 @@ data = loadtxt('Messwerte/aufg4', unpack='true')
 distance = (data[0]-3.6)/100 # abstand der Lampen in m
 output = data[1]/data[2] ## Out mit Gain normieren
 
-def intensity_fit(dist, factor,  const):
-    return factor*dist**-2+C
-
-curve_fit(intensity_fit, distance, output)
+def fit(x,a,b):
+    return a*x+b
 
 
 
-plot(regression_distance,regression_distance**-2 *params[1]+params[2])
+regression_distance = linspace(0.024,1.4)
+
+
 plot(distance,output,'x')
-
+yscale('log')
+xscale("log")
 show()
